219a5fafbb137215798e55daa092bcaf8fc3f962
madyaratri <ratriharwita@gmail.com>
2025-02-21 20:32:24 +0700

    Data Warehouse

---
+++ datamart_warehouse.sql
@@ -0,0 +1,16 @@
+CREATE TABLE datamart_education (
+    country TEXT,
+    completion_rate NUMERIC,
+    dropout_rate NUMERIC,
+    unemployment_rate NUMERIC,
+    year INTEGER,
+    gdp NUMERIC,
+    number_convicted_for_intentional_homicide INTEGER,
+    rate_per_100k_population NUMERIC,
+    latitude NUMERIC,
+    longitude NUMERIC,
+    region TEXT,
+    subregion TEXT,
+    country_code TEXT,
+    country_status TEXT
+);
